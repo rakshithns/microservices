d5cf36be699297daf8b5b58406ee130a69e869ec
rakshithns <rakshith10csstyle@gmail.com>
2023-09-17 01:46:08 +0530

    Configuring docker file for all the services

--- CarBidding/src/IdentityService/HostingExtensions.cs
+++ CarBidding/src/IdentityService/HostingExtensions.cs
@@ -30,6 +30,11 @@ internal static class HostingExtensions
 
                 // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
                 // options.EmitStaticAudienceClaim = true;
+
+                if (builder.Environment.IsEnvironment("Docker"))
+                {
+                    options.IssuerUri = "identity-svc";
+                }
             })
             .AddInMemoryIdentityResources(Config.IdentityResources)
             .AddInMemoryApiScopes(Config.ApiScopes)
